3060be04e405a08452399400595f8a7d7d69a9f2
ImJaycee <jayceecruz131@gmail.com>
2023-07-10 15:32:24 +0800

    Add dashboard page
    Finish Dashboard page responsive design

---
+++ pages/dashboard.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Apartment</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="../styles/dashboard.css">
+</head>
+<body>
+<section id="home">
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="/assets/dashboard-ast/house.jpg" alt="Logo" width="50px"><span></span></a>
+      
+      <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <form class="d-flex" role="search">
+          <div class="input-group">
+            <input class="form-control" type="search" placeholder="Search Room No." aria-label="Search">
+            <button class="btn btn-light btn-search" type="submit">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </form>
+        <ul class="navbar-nav ml-auto justify-content-end">
+          <li class="nav-item active">
+            <a class="nav-link" href="#home"><span><i class="fas fa-tachometer-alt"></i></span>Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span><i class="fas fa-dollar-sign"></i></span>Payment</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span><i class="fas fa-users"></i></span>Tenant</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span><i class="fas fa-user"></i></span>Account</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><span><i class="fas fa-sign-out-alt"></i></span>Logout</a>
+          </li>
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
+
+  <div class="main-content mt-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="box">
+            <a href="#">
+              <img src="/assets/dashboard-ast/house.jpg" alt="Room 1" class="room-img">
+              <div class="tenant-info">
+                <img src="/assets/dashboard-ast/profile.jpg" alt="Tenant Name" class="tenant-profile">
+                <h5>Tenant Name</h5>
+              </div>
+            </a>
+          </div>
+        </div>
+        
+</section>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
