e39b41d44b9f3e5bfee2932fe9743714452e7849
ImJaycee <jayceecruz131@gmail.com>
2023-07-11 21:12:51 +0800

    Adjust switch size responsive?

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,11 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"><img src="/assets/dashboard-ast/house.jpg" alt="Logo" width="50px"><span></span></a>
-        <button class=" btn-switch btn-mode" id="modeButton" type="button">
-            <div class="circle"></div>
-         </button>
+        
       <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      
       <div class="collapse navbar-collapse" id="navbarNav">
         <form class="d-flex" role="search">
           <div class="input-group">
@@ -16,6 +15,9 @@
             </button>
           </div>
         </form>
+        <button class=" btn-switch btn-mode" id="modeButton" type="button">
+            <div class="circle"></div>
+         </button>
         
         <ul class="navbar-nav ml-auto justify-content-end">
           <li class="nav-item active">
@@ -34,7 +36,8 @@
             <a class="nav-link" href="#"><span><i class="fas fa-sign-out-alt"></i></span>Logout</a>
           </li>
         </ul>
-       
+        
+
       </div>
     </div>
   </nav>
